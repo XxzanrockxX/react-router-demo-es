74b549428508d04a02482d4ef9c11a239d1b6643
zanrock <luisenin@hotmail.com>
2026-07-18 18:41:32 -0500

    feat: integrate react-router for navigation and update dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import Header from "./Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard/Dashboard.jsx";
+import AboutMe from "./AboutMe/AboutMe.jsx";
+import Reviews from "./Reviews/Reviews.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/reviews" element={<Reviews />} /> 
+      </Routes>
     </div>
   );
 }
