ee99f11d0a2129250a418a51158687ecf86d8e04
zanrock <luisenin@hotmail.com>
2026-07-21 17:31:41 -0500

    feat: enhance AboutMe and AboutUs components with new routes and content

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,19 @@
+import { Outlet, Link } from "react-router-dom";
 import "./AboutUs.css";
 
-function AboutUs() { 
+function AboutUs() {
   return (
     <div className="about-us">
       <p>Aquí puedes encontrar más información sobre nuestro sitio.</p>
+      <ul>
+        <li>
+          <Link to="mission">Nuestra misión</Link>
+        </li>
+        <li>
+          <Link to="history">Historia del sitio</Link>
+        </li>
+      </ul>
+      <Outlet />
     </div>
   );
 }
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,33 @@
-import "./App.css";
-import Header from "./Header/Header";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./Dashboard/Dashboard.jsx";
-import AboutMe from "./AboutMe/AboutMe.jsx";
-import Reviews from "./Reviews/Reviews.jsx";
-import AboutUs from "./AboutUs/AboutUs.jsx"; 
 
+import Header from "./Header/Header";
+import Dashboard from "./Dashboard/Dashboard";
+import Reviews from "./Reviews/Reviews";
+import AboutMe from "./AboutMe/AboutMe";
+import Contact from "./AboutMe/Contact";
+import Hobbies from "./AboutMe/Hobbies";
+import MyStory from "./AboutMe/MyStory";
+import AboutUs from "./AboutUs/AboutUs";
+import SiteHistory from "./AboutUs/SiteHistory";
+import SiteMission from "./AboutUs/SiteMission";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/reviews" element={<Reviews />} /> 
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="mission" element={<SiteMission />} />
+          <Route path="history" element={<SiteHistory />} />
+        </Route>
       </Routes>
     </div>
   );
